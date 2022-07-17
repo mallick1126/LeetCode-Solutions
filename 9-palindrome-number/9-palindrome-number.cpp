79390bc742796905769acd12bd17364716fedec9
Rahul Mallick <rahulmallick1126@gmail.com>
2022-07-17 12:15:48 +0530

    Time: 39 ms (12.85%), Space: 5.9 MB (31.51%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        
+        //using two pointers
         string num = to_string(x);
         int i = 0;
         int j = num.size()-1;
@@ -9,12 +11,5 @@ public:
             if(num[i++]!=num[j--])
                 return false;
         return true;
-        
-    
-        
-        // string s = to_string(x);
-        // int i = 0, j = s.size()-1;
-        // while (i <= j) if (s[i++] != s[j--]) return false;
-        // return true;
     }
 };
